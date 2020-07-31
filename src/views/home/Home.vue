0d73a676b8aa2c271a3a540f96ea7d77d6f51c5b
dsy <dushi1225@163.com>
2020-07-31 21:39:21 +0800

    请求首页的多个数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,20 +1,33 @@
 <template>
-<div id="home">
-  <nav-bar class="home-nav">
-    <div slot="center">购物街</div>
-  </nav-bar>
-</div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 <script>
 import navBar from "components/common/navBar/NavBar";
+import { getHomeMultidata } from "network/home";
 export default {
+  name: "Home",
+  data() {
+    return { banner: null, recommend: null };
+  },
   components: {
-    navBar
+    navBar,
+  },
+  created() {
+    getHomeMultidata().then((res) => {
+      this.banner = res.data.banner.list;
+      this.recommend = res.data.recommend.list;
+      console.log(this.banner);
+      console.log(this.recommend);
+    });
   },
 };
 </script>
 <style scoped>
-.home-nav{
+.home-nav {
   background-color: var(--color-tint);
   color: #fff;
 }
